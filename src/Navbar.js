1587d48f96d3096f42848d1a8f9204d36a850044
Neranjana Prasad | Nandiya <30690726+NandiyaLive@users.noreply.github.com>
2021-06-15 18:30:03 +0530

    Update Navbar.js

--- src/Navbar.js
+++ src/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
                         <Link to="/"><li>Home</li></Link>
                         <Link to="/about"><li>About</li></Link>
                         <Link to="/works"><li>Works</li></Link>
-                        <li><a href="https://blog.neranjana.ga">Blog</a></li>
+                        <li><a href="https://blog.neranjana.ga" target="_blank">Blog</a></li>
                         <li><p onClick={handleClick}><i className="fa fa-adjust"></i></p></li>
                     </ul>
                 </nav>
